f4c6fab958c929530d79702dafdccb608158651f
Taylor Skidmore <taylor@taylorskidmore.com>
2015-12-01 13:39:42 -0500

    add brute-force solver

--- Brute.hs
+++ Brute.hs
@@ -4,5 +4,27 @@ module Brute
 
 import Types
 
+lookupEnv :: Env -> Int -> Bool
+lookupEnv [] n = False -- we should never hit this
+lookupEnv (x:xs) n =
+  if fst x == n
+  then snd x
+  else lookupEnv xs n
+
+cleanLine :: Env -> Clause -> Bool
+cleanLine env clause =
+  foldr (||) False (map (\x -> if x < 0
+                               then not $ lookupEnv env (abs x)
+                               else lookupEnv env x) clause)
+
+checkEnv :: Env -> [Clause] -> Bool
+checkEnv env [] = True
+checkEnv env (x:xs) =
+  cleanLine env x && checkEnv env xs
+
 bruteSolve :: [Env] -> [Clause] -> Env
-bruteSolve (env:envs) (x:xs) = env
+bruteSolve [] x = []
+bruteSolve (x:xs) clauses =
+  if checkEnv x clauses
+  then x
+  else bruteSolve xs clauses
